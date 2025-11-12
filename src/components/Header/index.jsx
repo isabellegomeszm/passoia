29fdc182ed65cd26183943eaef2c2ed6a5ea1d67
isabellegomeszm <isabellegomeszm@gmail.com>
2025-11-12 10:46:07 -0300

    First commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+
+
+function Header() {
+    return (
+        <header>
+            <img src="" alt="logo marca da página" />
+            <ul>
+                <li>LOOKS</li>
+                <li>LANÇAMENTOS</li>
+                <li>NOVIDADES</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
